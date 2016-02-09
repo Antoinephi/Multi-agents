ce9c93992ff79bdb3c1cb3b9f5b94655bc834238
Mandrivia <antoine.philippe62@gmail.com>
2016-02-09 18:52:33 +0100

    removed head

--- src/particles/Main.java
+++ src/particles/Main.java
@@ -11,11 +11,7 @@ public class Main {
 	private static final int SIM_SPEED = 30;
 	private static final int NB_TURNS = 10000;
 	private static final boolean TORIC = true;
-<<<<<<< HEAD
-	private static final boolean INFINITE_MODE = false;
-=======
 	public static final boolean INFINITE_MODE = true;
->>>>>>> c77874ef1dfacc28cdbbf1afc83e8182d35cbf5e
 	private static final boolean LOGGING = false;
 	private static final boolean FAIR_MODE = true;
 	private static final boolean SHOW_GRID = true;
